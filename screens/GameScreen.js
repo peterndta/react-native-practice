7725ccc818d02f1dd418f0d2067781639c61dd99
Peter <truonganh20062001@gmail.com>
2022-12-13 00:05:27 +0700

    Finish style design

---
+++ screens/GameScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+function GameScreen() {
+  return (
+    <View>
+      <Text>Opponents's Guess</Text>
+    </View>
+  )
+}
+
+export default GameScreen
